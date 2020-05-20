e1c4970137c01c5cb32ab667a7be6296ee9ecb9c
Bojan Misic <bojan.misic@banana.rs>
2020-05-20 11:37:03 +0200

    Initial webpage commit

---
+++ script.js
@@ -0,0 +1,33 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("menu-icon").style.display = "none";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("menu-icon").style.display = "inherit";
+}
+
+function adjust_textarea(h) {
+    h.style.height = "20px";
+    h.style.height = (h.scrollHeight)+"px";
+}
+
+/* */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('.page').forEach(page => {
+    page.addEventListener('click', function (e) {
+        closeNav();
+    });
+});

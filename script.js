2d83bee88c1e97b530726d1eb6acc86f806012c9
Ivan Stamenic <ivanstamenic@gmail.com>
2020-06-29 22:36:48 -0600

    set mode to script

--- script.js
+++ script.js
@@ -41,9 +41,18 @@ document.querySelector("#contact-form").addEventListener("submit", function(e){
         messageBody: e.target["messageBody"].value
     };
 
-    fetch("https://a7-send-email.azurewebsites.net/api/SendEmailA7?code=TfZZcTJeH5oFdByV/bnJps2WDbdnmohhbe9Wfzy65yziGB3Qf4OJFA==", {
-        method: "POST", 
-        body: JSON.stringify(data)
+    var response = await fetch("https://a7-send-email.azurewebsites.net/api/SendEmailA7?code=TfZZcTJeH5oFdByV/bnJps2WDbdnmohhbe9Wfzy65yziGB3Qf4OJFA==", {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'include', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
       }).then(res => {
         console.log("Request complete! response:", res);
       });
